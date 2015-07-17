fea22908b7462f1d96fc5103252fbe58ae1d7db9
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-17 18:22:44 +0200

    Initial stubs for our dsa-board software.

---
+++ media-control.py
@@ -0,0 +1,80 @@
+# Copyricht (C) 2015 by the Schwarzenholz Birthday Connection
+
+#!/usr/bin/python3
+# coding=utf8
+
+##############################################################
+# PARAMETERS:
+##############################################################
+
+# Maps GPIO pin numbers to button indices
+# TODO: Fill in proper button channels here!
+button_indices = {1 : 0
+				  2 : 1}
+
+# A list of full paths to directories that are to be searched
+# for media.
+# Search happens recursively. All media files that are contained
+# in a directory the name of which can be interpreted numerically
+# are assigned to the button that has this numerical interpretation
+# as its index
+media_dirs = ["/media"]
+            
+# Name suffices of files that are to be recognized as media files
+extensions = (".wav", ".mp2", ".mp3", ".mp4", ".mpeg")
+
+##############################################################
+
+log("Importing python modules...")
+
+# Helper functions:
+from util import *
+
+# This API gives access to the "General Purpose Input/Output"
+# of the Pi board
+import RPi.GPIO as GPIO
+
+
+# For handling filesystem paths.
+import os.path
+
+logLine("Done.")
+
+log("Initializing pins for button input...")
+ # Make sure that pins are numbered according to the P1 header of the Pi board.
+GPIO.setmode(GPIO.BOARD)
+
+# Set our button channels to input mode:
+for chn in button_channels:
+	GPIO.setup(ch, GPIO.IN)
+logLine("Done.")
+
+
+
+log("Indexing storage media...")
+media = {} # A dict mapping button indices (int) to lists of media file paths
+fc = 0 # The number of files that have been recognized as media files.
+
+# Search for media:
+for root in media_dirs:
+	for dirpath, _, filenames in os.walk(root)
+		
+		dirname = os.path.dirname(dirpath)
+
+		buttonIndex = None
+		try:
+			buttonIndex = interpretNumerically(dirname)
+		except ValueError:
+			continue
+		
+		for fn in filenames:
+			if fn.endswith(extensions):
+				media[buttonIndex].append(os.path.join(dirpath, fn))
+				fc += 1
+
+logLine("Done. {fc} files have been selected.".format(fc=fc))
+
+playRandomMedium(media[0])
+
+when button pressed:
+	playRandomMedium(media[buttonIndex])
---
+++ util.py
@@ -0,0 +1,45 @@
+# This file contains helper functions for media-control.py
+# Copyricht (C) 2015 by the Schwarzenholz Birthday Connection
+
+# coding=utf8
+
+# For randomness:
+import random
+random.seed()
+
+
+def log(msg):
+	print(msg, end='')
+
+
+def logLine(msg):
+	log(msg)
+	log('\n')
+
+
+
+def interpretNumerically(fileName):
+	'''
+	Tries to interpret the given file name as a number and
+	returns this number. If interpretation fails, a ValueError
+	is raised.
+	'''
+	
+	try:
+		return int(fileName)
+	except:
+		raise ValueError("Could not interpret the file name '{fileName}' as a number!".format(fileName=fileName)
+
+
+def playRandomMedium(mediaPaths):
+	'''
+	Randomly selects one of the given media paths and plays the designated file.
+	The given iterable must contain full paths.
+	'''
+	try:
+		playMedium(random.choice(mediaPaths)
+	except IndexError:
+		pass
+
+def playMedium(filePath):
+	pass
