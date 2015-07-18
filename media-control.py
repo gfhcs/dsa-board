6bca0c0b9a91a90559ae2f6f35e57c333df7e4b9
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-07-18 20:34:59 +0200

    Moved the delay in front of the indexing, in order to allow some time for mounting the USB drive.

--- media-control.py
+++ media-control.py
@@ -12,35 +12,41 @@ from util import *
 # For sleeping between button events.
 import time
 
-log("Indexing storage media...")
+log("Waiting for mounting")
+time.sleep(20) # Delay for mounting.
+logLine("Done")
 
-fc = 0 # The number of files that have been recognized as media files.
 
-# Search for media:
-for root in media_dirs:
-	for dirpath, _, filenames in os.walk(root):
-		
-		dirname = os.path.basename(dirpath)
+def index():
+	log("Indexing storage media...")
+	
+	fc = 0 # The number of files that have been recognized as media files.
+	
+	# Search for media:
+	for root in media_dirs:
+		for dirpath, _, filenames in os.walk(root):
+			
+			dirname = os.path.basename(dirpath)
+	
+			buttonIndex = None
+			try:
+				buttonIndex = interpretNumerically(dirname)
+			except ValueError:
+				continue
+			
+			for fn in filenames:
+				if fn.endswith(media_extensions):
+					try:
+						buttons[buttonIndex].assignMedium(os.path.join(dirpath, fn))
+						fc += 1
+					except KeyError:
+						pass
+	
+	logLine("Done. {fc} files have been selected.".format(fc=fc))
 
-		buttonIndex = None
-		try:
-			buttonIndex = interpretNumerically(dirname)
-		except ValueError:
-			continue
-		
-		for fn in filenames:
-			if fn.endswith(media_extensions):
-				try:
-					buttons[buttonIndex].assignMedium(os.path.join(dirpath, fn))
-					fc += 1
-				except KeyError:
-					pass
-
-logLine("Done. {fc} files have been selected.".format(fc=fc))
+index()
 
 # Start listening for button events:
-
-time.sleep(3) # Delay until Pi's audio system is up.
 
 # Play a welcome medium:
 buttons[0].play()
